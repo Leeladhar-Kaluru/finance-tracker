c151daab5955fe66ce6229ac5457f4ec46c188ba
leeladhar <leeladhar2804@gmail.com>
2026-06-18 13:06:44 +0530

    Implemented CRUD operations using MongoDB atlas

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -18,4 +18,97 @@ const createTransaction = async (req,res)=>{
     }
 };
 
-module.exports = {createTransaction};
+const getTransactions = async (req,res)=>{
+    try{
+        const transactions = await Transaction.find();
+        res.status(201).json({
+            success:true,
+            data:transactions
+        });
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+const getTransactionById = async (req,res)=>{
+    try{
+        const {id} = req.params;
+        const transaction = await Transaction.findById(id);
+        if(!transaction){
+            return res.status(404).json({
+                success:false,
+                message:"Transaction not found"
+            });
+        }
+        else{
+            res.status(201).json({
+                success:true,
+                data:transaction
+            });
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+const deleteTransaction = async (req,res)=>{
+    try{
+        const {id} = req.params;
+        const transaction = await Transaction.findByIdAndDelete(id);
+        if(!transaction){
+            return res.status(404).json({
+                success:false
+            })
+        }
+        else{
+            res.status(201).json({
+                success:true,
+                message:"Transaction deleted successfully"
+            });
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+const updateTransaction = async (req,res)=>{
+    try{
+        const {id} = req.params;
+        const transaction = await Transaction.findByIdAndUpdate(id, req.body, {
+            new:true,
+            runValidators:true});
+        if(!transaction){
+            return res.status(404).json({
+                success:false,
+                message:"Transaction not found"
+            });
+        }
+        else{
+            res.status(201).json({
+                success:true,
+                data:transaction,
+                message:"Transaction updated successfully"
+            });
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        });
+    }
+}
+
+module.exports = {createTransaction, getTransactions, getTransactionById, deleteTransaction, updateTransaction};
--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const {createTransaction} = require("../controllers/transactionController");
+const {createTransaction, getTransactions, getTransactionById, deleteTransaction, updateTransaction} = require("../controllers/transactionController");
 const router = express.Router();
 
 router.post("/", createTransaction);
+router.get("/", getTransactions);
+router.get("/:id", getTransactionById);
+router.put("/:id", updateTransaction);
+router.delete("/:id", deleteTransaction);
 module.exports = router;
